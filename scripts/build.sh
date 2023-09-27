b1deac3db32fa7b85d1fdf27d23ad8f8bafbcff5
ShehabM0 <shehabmohamed2357@gmail.com>
2023-09-27 23:32:56 +0300

    Update

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+BOLDGREEN="\e[1;32m"
 clear
 g++ $1.cpp -o $1
-echo "Compiled and running..."
+echo -e "${BOLDGREEN}Compilled and running...${ENDCOLOR}"
 ./$1
