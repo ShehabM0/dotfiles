1d0d642b6a4bab4ded6ebaaad141441e4ff39bf5
ShehabM0 <shehabmohamed2357@gmail.com>
2024-04-09 23:37:23 +0200

    Update

---
+++ vim/template.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+void solve() {
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+    // int t; cin >> t; while(t--) solve();
+    solve();
+}
+
