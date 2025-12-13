4f257ae93c4d059c89d82c94340187eda559c0a7
ShehabM0 <shehabmohamed2357@gmail.com>
2025-12-13 19:27:22 +0200

    Update

--- scripts/subtitle_merge.sh
+++ scripts/subtitle_merge.sh
@@ -6,9 +6,14 @@ SUBTITLE="$2"
 BASENAME=$(basename "$MOVIE" .mp4)
 OUTPUT="${BASENAME}_with_sub.mp4"
 
+# CPU-based
+# ffmpeg -i "$MOVIE" \
+# -vf "subtitles='$SUBTITLE':force_style='fontname=Noto Sans Arabic Medium,FontSize=42'" \
+# -c:a copy "$OUTPUT"
 
-ffmpeg -i "$MOVIE" \
--vf "subtitles='$SUBTITLE':force_style='fontname=Noto Sans Arabic Medium,FontSize=38'" \
--c:a copy "$OUTPUT"
+# GPU-based
+ffmpeg -hwaccel cuda -i "$MOVIE" \
+-vf "subtitles='$SUBTITLE':force_style='fontname=Noto Sans Arabic Medium,FontSize=42'" \
+-c:v h264_nvenc -preset fast -c:a copy "$OUTPUT"
 
 echo "Subtitle merged into $OUTPUT"
